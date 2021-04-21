2cc7c2b12787e3e6c31b31a5f376ae5bf07c1ad6
Nick Cannariato <434063+birdcar@users.noreply.github.com>
2021-04-21 11:29:40 -0500

    Update gatsby config to include MDX blog posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,30 +1,39 @@
 module.exports = {
   siteMetadata: {
-    title: "birdcar",
-    siteUrl: "https://www.birdcar.dev",
+    title: `birdcar`,
+    github: `birdcar`,
+    siteUrl: `https://www.birdcar.dev`,
   },
   plugins: [
-    "gatsby-plugin-image",
-    "gatsby-plugin-react-helmet",
-    "gatsby-plugin-sitemap",
-    "gatsby-plugin-mdx",
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
+    `gatsby-plugin-image`,
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    `gatsby-plugin-mdx`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "images",
-        path: "./src/images/",
+        name: `images`,
+        path: `./src/images/`,
       },
-      __key: "images",
+      __key: `images`,
     },
     {
-      resolve: "gatsby-source-filesystem",
+      resolve: `gatsby-source-filesystem`,
       options: {
-        name: "pages",
-        path: "./src/pages/",
+        name: `pages`,
+        path: `./src/pages/`,
       },
-      __key: "pages",
+      __key: `pages`,
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `./posts/`,
+      },
+      __key: `posts`,
     },
   ],
 };
