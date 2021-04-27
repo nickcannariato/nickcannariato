f69ee5c9ee5547ccab43068b983a42fe6297ebb1
Nick Cannariato <434063+birdcar@users.noreply.github.com>
2021-04-27 09:43:03 -0500

    Configure Gatsby to source GraphQL data from GitHub

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,18 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-postcss`,
     {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        typeName: `GitHub`,
+        fieldName: `github`,
+        url: `https://api.github.com/graphql`,
+        headers: {
+          Authorization: `Bearer ${process.env.GH_PERSONAL_TOKEN}`,
+          "GraphQL-Features": `discussions_api`,
+        },
+      },
+    },
+    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
